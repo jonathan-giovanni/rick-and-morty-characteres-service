18d38252c67d00dd39ce49116ee2f7f4ae759e3a
jonathan <hv12i04001@gmail.com>
2023-03-06 20:47:44 +0100

    Initial project

---
+++ src/main/java/com/hv/rickandmorty/characteres/service/RickAndMortyCharacteresServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hv.rickandmorty.characteres.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyCharacteresServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyCharacteresServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hv/rickandmorty/characteres/service/RickAndMortyCharacteresServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hv.rickandmorty.characteres.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyCharacteresServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
