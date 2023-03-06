2deacea465fc7c4e296e9c5626d24766c8429670
jonathan <hv12i04001@gmail.com>
2023-03-06 22:36:12 +0100

    Fix rename test

--- src/main/java/com/hv/rickandmorty/characteres/service/application/SearchCharacterByName.java
+++ src/main/java/com/hv/rickandmorty/characteres/service/application/SearchCharacterByName.java
@@ -3,7 +3,9 @@ package com.hv.rickandmorty.characteres.service.application;
 import com.hv.rickandmorty.characteres.service.domain.exception.CharacterNotFoundException;
 import com.hv.rickandmorty.characteres.service.domain.model.Character;
 import com.hv.rickandmorty.characteres.service.domain.port.SearchCharacterAppearanceRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SearchCharacterByName {
 
     private final SearchCharacterAppearanceRepository searchCharacterAppearanceRepository;
--- src/test/java/com/hv/rickandmorty/characteres/service/infrastructure/adapter/graphql/GraphQLSearchCharacterAppearanceTest.java
+++ src/test/java/com/hv/rickandmorty/characteres/service/infrastructure/adapter/graphql/GraphQLSearchCharacterAppearanceTest.java
@@ -1,7 +1,6 @@
 package com.hv.rickandmorty.characteres.service.infrastructure.adapter.graphql;
 
 import graphql.Assert;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +14,13 @@ public class GraphQLSearchCharacterAppearanceTest {
     void searchRickSanchezExpectedResultInformation() {
         var graphQLSearchCharacterAppearance = new GraphQLSearchCharacterAppearance();
 
-        String name = "birdperson";
+        String name = "rick sanchez";
 
         var result = graphQLSearchCharacterAppearance.findByName(name);
 
         log.info("Searching: {} is present: {}",name,result.isPresent());
 
-        Assert.assertTrue(true);
+        Assert.assertTrue(result.isPresent());
     }
 
 }
